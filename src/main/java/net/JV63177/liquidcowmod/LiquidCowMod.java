06bfea626b2ffaa31d5c13239c7463f638821e90
Jonathan <valbertjonathan631@gmail.com>
2023-04-08 23:20:19 +0200

    Setup of Forge, Gradle and Parchment

--- src/main/java/net/JV63177/liquidcowmod/LiquidCowMod.java
+++ src/main/java/net/JV63177/liquidcowmod/LiquidCowMod.java
@@ -1,8 +1,6 @@
-package com.github.JV63177;
+package net.JV63177.liquidcowmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -10,9 +8,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(LiquidCowMod.MOD_ID)
@@ -21,26 +17,23 @@ public class LiquidCowMod {
     public static final String MOD_ID = "liquidcowmod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public LiquidCowMod() {
+
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         modEventBus.addListener(this::commonSetup);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("HELLO FROM COMMON SETUP");
-    }
 
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("HELLO from server starting");
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
-
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            LOGGER.info("HELLO FROM CLIENT SETUP");
+
         }
     }
 }
